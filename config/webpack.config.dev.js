5d2eb62d8b99e8f98b839f79304b59823a475e13
Wei Zhu <yesmeck@gmail.com>
2017-03-28 18:20:20 +0800

    Fix static path

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -192,7 +192,7 @@ module.exports = {
     new InterpolateHtmlPlugin(env.raw),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
-      inject: true,
+      inject: false,
       template: paths.appHtml,
     }),
     // Makes some environment variables available to the JS code, for example:
