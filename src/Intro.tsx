640c4e021c43c9c320712f9b976fd9db8b5a194c
Wei Zhu <yesmeck@gmail.com>
2017-03-28 13:59:09 +0800

    Add PolicyModal

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { SyntheticEvent } from 'react';
+import PolicyModal from './PolicyModal';
 
 const styles: any = require('./Intro.less');
 
-export default function Intro() {
-  return (
-    <div className={styles.intro}>
-      <h2>Before You Start...</h2>
-      <p>
-        The issue list is reserved exclusively for bug reports and feature requests. That means we do not accept usage questions. If you open an issue that does not conform to the requirements, <strong>it will be closed immediately</strong>. <a href="#modal">Why are we so strict about this?</a>
-      </p>
-      <p>
-        For usage questions, please use the following resources:
-      </p>
-      <ul>
-        <li>Read the <a href="https://ant.design/docs/react/introduce-cn" target="_blank">docs</a></li>
-        <li>Look for / ask questions on <a href="https://stackoverflow.com/questions/ask?tags=antd" target="_blank">StackOverflow</a></li>
-      </ul>
-      <p>
-        Also try to search for your issue - it may have already been answered or even fixed in the development branch. However, if you find that an old, closed issue still persists in the latest version, you should open a new issue using the form below instead of commenting on the old issue.
-      </p>
-    </div>
-  );
+export interface IntroState {
+  modalVisible: boolean;
+}
+
+export default class Intro extends React.Component<null, IntroState> {
+  state = {
+    modalVisible: false,
+  };
+
+  handleClick = (e: SyntheticEvent<HTMLElement>) => {
+    e.preventDefault();
+    this.setState({ modalVisible: true });
+  }
+
+  handleCancel = () => {
+    this.setState({ modalVisible: false });
+  }
+
+  render() {
+    const { modalVisible } = this.state;
+
+    return (
+      <div className={styles.intro}>
+        <PolicyModal visible={modalVisible} onCancel={this.handleCancel} />
+        <h2>Before You Start...</h2>
+        <p>
+          The issue list is reserved exclusively for bug reports and feature requests. That means we do not accept usage questions. If you open an issue that does not conform to the requirements, <strong>it will be closed immediately</strong>. <a href="#" onClick={this.handleClick}>Why are we so strict about this?</a>
+        </p>
+        <p>
+          For usage questions, please use the following resources:
+        </p>
+        <ul>
+          <li>Read the <a href="https://ant.design/docs/react/introduce-cn" target="_blank">docs</a></li>
+          <li>Look for / ask questions on <a href="https://stackoverflow.com/questions/ask?tags=antd" target="_blank">StackOverflow</a></li>
+        </ul>
+        <p>
+          Also try to search for your issue - it may have already been answered or even fixed in the development branch. However, if you find that an old, closed issue still persists in the latest version, you should open a new issue using the form below instead of commenting on the old issue.
+        </p>
+      </div>
+    );
+  }
 }
---
+++ src/PolicyModal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Modal } from 'antd';
+
+const styles: any = require('./PolicyModal.less');
+
+export interface PolicyModalProps {
+  visible: boolean;
+  onCancel: () => any;
+}
+
+export default function PolicyModal({ visible, onCancel }: PolicyModalProps) {
+  return (
+    <Modal
+      title="The reason behind our strict issue policy"
+      footer=""
+      visible={visible}
+      onCancel={onCancel}
+      width="680"
+    >
+      <div className={styles.policy}>
+        <p>As a free and open source project, Ant Design also has limited maintainer bandwidth. That means the only way to ensure the project's sustainability is to:</p>
+        <ol>
+          <li>Prioritize more concrete work (bug fixes and new features);</li>
+          <li>Improve issue triaging efficiency.</li>
+        </ol>
+        <p>For (1), we have decided to use the GitHub issue lists exclusively for work that has well-defined, actionable goals. Questions and open ended discussions should be posted to mediums that are better suited for them.</p>
+        <p>For (2), we have found that issues that do not provide proper information upfront usually results in terribly inefficient back-and-forth communication just to extract the basic information needed for actual triaging. This is exactly why we have created this app: to ensure that every issue is created with the necessary information, and to save time on both sides.</p>
+      </div>
+    </Modal>
+  );
+}
