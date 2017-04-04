9d993dbc06f96c87fa48c2853779fadd2c435f72
Wei Zhu <yesmeck@gmail.com>
2017-04-04 23:34:11 +0800

    Check response status

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,23 @@ const compareVersions: any = require('compare-versions');
 
 interface Response {
   json(): any;
+  status: number;
+  statusText: string;
 }
 
 const endpoint = 'https://api.github.com';
 
+function checkStatus(response: Response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(response.statusText);
+  }
+}
+
 export function fetchVersions(repo: string) {
   return fetch(`${endpoint}/repos/ant-design/${repo}/releases`)
+    .then(checkStatus)
     .then((response: Response) => response.json())
     .then(releases => releases.map((r: any) => r.tag_name))
     .then(versions => versions.sort((a: string, b: string) => -compareVersions(a, b)));
@@ -16,6 +27,7 @@ export function fetchVersions(repo: string) {
 export function fetchIssues(repo: string, keyword: string) {
   const q = encodeURIComponent(`is:issue repo:ant-design/${repo} ${keyword}`);
   return fetch(`${endpoint}/search/issues?q=${q}&per_page=5`)
+    .then(checkStatus)
     .then((response: Response) => response.json())
     .then(json => json.items);
 }
