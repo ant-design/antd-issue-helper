f45ce8da528a06b7eea8d5a323713dfd7d77371a
Wei Zhu <yesmeck@gmail.com>
2017-12-04 20:00:45 +0800

    New icon

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ class App extends React.Component<{}, AppState> {
             <div className={styles.logo}>
               <img
                 alt="logo"
-                src="https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg"
+                src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
               />
               <h1>
                 Issue Helper
