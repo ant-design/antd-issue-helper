e90dda67c2157a843bb90c4e9d142560929f7f3e
afc163 <afc163@gmail.com>
2021-10-12 17:28:15 +0800

    Update

---
+++ precache-manifest.ff781ed3de68f670749003c9c4cccbd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3411ae2b046df2b613fee4c09ff6167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c799f90c7c0f7fda078a",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "0380eddbed30a9301668",
+    "url": "/static/css/main.706f0aeb.chunk.css"
+  },
+  {
+    "revision": "c799f90c7c0f7fda078a",
+    "url": "/static/js/2.694b1996.chunk.js"
+  },
+  {
+    "revision": "5eb8a1f43a1a98f94fd11dc351ab6fb6",
+    "url": "/static/js/2.694b1996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0380eddbed30a9301668",
+    "url": "/static/js/main.577221a3.chunk.js"
+  },
+  {
+    "revision": "1902cc1d603fc2cb64f9",
+    "url": "/static/js/runtime-main.37957515.js"
+  }
+]);
