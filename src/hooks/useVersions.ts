0e34cb39881a5022dfaf75a19ffa080b78c32352
afc163 <afc163@gmail.com>
2022-03-23 18:44:00 +0800

    fix: form rerender infinitely

--- src/hooks/useVersions.ts
+++ src/hooks/useVersions.ts
@@ -1,18 +1,23 @@
 import * as api from "../api";
 import { state } from "reactive.macro";
+import { useCallback } from "react";
 
 export default function useVersions() {
   let repoVersions: { [repo: string]: string[] } = state({});
 
-  const fetchVersions = (repo: string) => {
-    api.fetchVersions(repo).then(
-      (versions: string[]) =>
-        (repoVersions = {
-          ...repoVersions,
-          [repo]: versions
-        })
-    );
-  };
+  const fetchVersions = useCallback(
+    (repo: string) => {
+      api.fetchVersions(repo).then(
+        (versions: string[]) =>
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          (repoVersions = {
+            ...repoVersions,
+            [repo]: versions
+          })
+      );
+    },
+    [JSON.stringify(repoVersions)]
+  );
 
   return { repoVersions, fetchVersions };
 }
